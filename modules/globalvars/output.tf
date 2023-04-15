bb1ff0828e57b463bcfad23bfdeed5bac2c0ac07
aparna2271 <130799812+aparna2271@users.noreply.github.com>
2023-04-15 03:30:43 -0400

    Added autoscaling_group and globalvars

---
+++ modules/globalvars/output.tf
@@ -0,0 +1,11 @@
+output "default_tags" {
+  value = {
+    "App"   = "Two-Tier Web Application"
+    "Onwer" = "Ashmit, Ish, Tarun, Ipsa, Aparna"
+  }
+}
+
+
+output "prefix" {
+  value     = "Group17"
+}
